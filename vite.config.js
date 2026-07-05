95d65f3c7b39cc315d3e7ceb3a55cebcbc5a6a6b
huongque0612 <hcjzr22jpz@privaterelay.appleid.com>
2026-07-06 05:15:58 +0700

    Add Firebase config and GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === "true" ? "/project-web-1/" : "/",
   plugins: [react()],
   build: {
     rollupOptions: {
